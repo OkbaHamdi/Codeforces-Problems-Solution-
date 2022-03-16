28357be81a93c0cfb602fef88e317f995c9e2c6b
Okba Hamdi <94719238+OkbaHamdi@users.noreply.github.com>
2022-03-16 16:22:33 +0100

    Create Xor-tree.cpp

---
+++ 429A/Xor-tree.cpp
@@ -0,0 +1,115 @@
+   /************      Author :
+
+                L_7
+
+            OKBA HAMDI
+
+                           **************/
+#include<bits/stdc++.h>
+using namespace std;
+
+    /***************Typedef *************/
+
+typedef         long long ll;
+typedef         long double ld;
+typedef         vector<ll>vl;
+typedef         deque<ll>dq;
+typedef         pair<ll,ll>pi;
+typedef         priority_queue<ll>pq;
+typedef         priority_queue<ll,vector<ll>,greater<ll>>pqg;
+
+
+    /********** Constantes **************/
+
+const           ll MOD = 1000000007;
+const           ll eps=1e-9;
+const           double PI=acos(-1);
+const           ll N=1e5+5;
+const           ll INF=1e18;
+            /************  Define  ****************/
+
+#define         F first
+#define         S second
+#define         pb push_back
+#define         ppb pop_back
+#define         pf push_front
+#define         ppf pop_front
+#define         sz(x)  (ll)((x).size())
+#define         rep(i,a,b) for(ll i=a;i<b;i++)
+#define         L_Headshot_7 ios_base::sync_with_stdio(false);cin.tie(0);
+#define         aff(v) for(auto e:v) cout<<e<<endl;cout<<endl;
+#define         all(v) v.begin(), v.end()
+#define         rall(v) v.rbegin(), v.rend()
+#define         READ freopen("input.txt", "r",stdin)
+#define         WRITE freopen("output.txt", "w",stdout)
+
+            /***************** Useful Functions *********************/
+
+ll fact(ll x){return(x<2?1:x*fact(x-1));}
+ll binexp(ll a,ll k){ll ans=1;while(k){if(k&1)ans=(ans*a)%MOD;a=(a*a)%MOD;k>>=1;}return ans;}
+ll gcd(ll a, ll b){while (b) swap(b, a %= b);return a;}inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }ll ext(ll a, ll b, ll& x, ll& y){x = 1, y = 0;ll x1 = 0, y1 = 1, a1 = a, b1 = b;while (b1){ll q = a1 / b1;tie(x, x1) = make_pair(x1, x - q * x1);tie(y, y1) = make_pair(y1, y - q * y1);tie(a1, b1) = make_pair(b1, a1 - q * b1);}return a1;}
+
+                /******************Code*********************/
+vl graph[N],ans,current,target;
+bool vis[N],visited[N];
+ll lvl[N];
+void golden_dfs(ll s,ll p,ll imp){
+    if(visited[s])return;
+    visited[s]=1;
+    if(lvl[s]%2){
+        ll a=(current[s]+imp)%2;
+        if(target[s]!=a){ans.pb(s);imp++;}
+    }
+    else{
+        ll a=(current[s]+p)%2;
+        if(target[s]!=a){ans.pb(s);p++;}
+    }
+    for(auto x:graph[s])golden_dfs(x,p,imp);
+}
+void dfs(ll s,ll val){
+    if(vis[s])return;
+    vis[s]=1;
+    lvl[s]=val;
+    for(auto x:graph[s]){
+        dfs(x,val+1);
+    }
+}
+void solve(){
+    ll n;
+    cin>>n;
+    rep(i,1,n){
+        ll a,b;
+        cin>>a>>b;
+        graph[a].pb(b);
+        graph[b].pb(a);
+    }
+
+    current.pb(0);
+    target.pb(0);
+    for(ll i=1;i<=n;i++){
+        ll a;
+        cin>>a;
+        current.pb(a);
+    }
+    for(ll i=1;i<=n;i++){
+        ll a;
+        cin>>a;
+        target.pb(a);
+    }
+    dfs(1,1);
+    golden_dfs(1,0,0);
+    cout<<ans.size()<<endl;
+    aff(ans);
+}
+int main()
+{
+    L_Headshot_7;
+    //READ;
+    //WRITE;
+    //ll test;
+    //cin>>test;
+    //while(test--){
+        solve();
+       //}
+    return 0;
+}
