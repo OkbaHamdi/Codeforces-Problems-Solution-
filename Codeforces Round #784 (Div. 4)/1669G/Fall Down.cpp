fbdca4e72e26edaed9463573fb98d60576a1e51b
Okba Hamdi <94719238+OkbaHamdi@users.noreply.github.com>
2022-04-21 23:48:35 +0100

    Create Fall Down.cpp

---
+++ Codeforces Round #784 (Div. 4)/1669G/Fall Down.cpp
@@ -0,0 +1,76 @@
+/************ Author :OKBA HAMDI**************/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    /***************Typedef *************/
+
+typedef         long long ll;
+typedef         long double ld;
+typedef         vector<ll>vl;
+typedef         deque<ll>dq;
+typedef         pair<ll,ll>pl;
+typedef         priority_queue<ll>pq;
+typedef         priority_queue<ll,vector<ll>,greater<ll>>pqg;
+
+
+    /********** Constantes **************/
+
+const           ll MOD = /*998244353;//*/1000000007;
+const           ll eps=1e-9;
+const           double PI=acos(-1);
+const           ll N=1e6+5;
+const           ll INF=1e18;
+
+            /************  Define  ****************/
+
+#define         F first
+#define         S second
+#define         pb push_back
+#define         ppb pop_back
+#define         pf push_front
+#define         ppf pop_front
+#define         sz(x)  (ll)((x).size())
+#define         rep(i,a,b) for(ll i=a;i<b;i++)
+#define         KIRA ios_base::sync_with_stdio(false);cin.tie(0);
+#define         aff(v) for(auto e:v) cout<<e<<" ";cout<<endl;
+#define         all(v) v.begin(), v.end()
+#define         rall(v) v.rbegin(), v.rend()
+#define         READ freopen("input.txt", "r",stdin)
+#define         WRITE freopen("output.txt", "w",stdout)
+
+            /***************** Useful Functions *********************/
+ll fact(ll x){return(x<2?1:x*(fact(x-1))%MOD);}
+ll binexp(ll a,ll k){ll ans=1;while(k){if(k&1)ans=(ans*a)%MOD;a=(a*a)%MOD;k>>=1;}return ans;}
+ll gcd(ll a, ll b){while (b) swap(b, a %= b);return a;}
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll C(ll n,ll k){return (fact(n)/(fact(k)*fact(n-k)));}
+string yup(ll a,ll b){if(a%b==0)return to_string(a/b);string ch=to_string(a/b);ll c=a%b;ll g=gcd(c,b);c/=g;b/=g;ch+="+"+to_string(c)+"/"+to_string(b);return ch;}
+                /******************Code******************/
+void solve(){
+    ll n,m;
+    cin>>n>>m;
+    map<ll,string>mp;
+    rep(i,0,n)cin>>mp[i];
+    for(ll j=0;j<m;j++){
+        for(ll i=n-2;i>=0;i--){
+            if(mp[i][j]=='o'||mp[i][j]=='.')continue;
+            for(ll k=i+1;k<n;k++){
+                if(mp[k][j]=='o'||mp[k][j]=='*')break;
+                mp[k][j]='*';
+                mp[k-1][j]='.';
+            }
+        }
+    }
+    for(auto x:mp)cout<<x.S<<endl;
+}
+
+    int main()
+    {
+        KIRA;
+        //READ;
+        //WRITE;
+        ll tests = 1;
+        cin >> tests;
+        while(tests--)solve();
+    }
